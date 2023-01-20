a39e9a576b26d2e0ea72fbef2e30b79d3a7153e1
yoonjeong-choi-dev <yjchoi7166@gmail.com>
2023-01-21 01:11:17 +0900

    Change the structure of the repository

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 * Moloco 회사 업무 관련 스터디
 * 학습한 내용 정리 및 실습 코드 저장
 * Contents
-  * Frontend
-  * Ad Tech
+  * Frontend - 프론트엔드
+  * Code Maintenance - 유지보수(코드 품질, 테스팅)
+  * Ad Tech - 도메인 지식
 
--- refactoring-2e/src/ch1-first-refactoring/loadData.mjs
+++
@@ -1,4 +0,0 @@
-import { readFile } from 'fs/promises';
-
-export const plays = JSON.parse(await readFile('./data/plays.json', 'utf8'));
-export const invoices = JSON.parse(await readFile('./data/invoices.json', 'utf8'));
