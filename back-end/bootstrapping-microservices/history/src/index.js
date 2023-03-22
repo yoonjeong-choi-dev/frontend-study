6d39de5efdee721c77a450e2c11d72710bb40991
yoonjeong-choi-dev <yjchoi7166@gmail.com>
2023-03-22 11:47:58 +0900

    [TIL] Chapter 5. Direct & Indirect Messaging

--- back-end/bootstrapping-microservices/history/src/index.js
+++ back-end/bootstrapping-microservices/history/src/index.js
@@ -1,11 +1,81 @@
 const express = require('express');
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
+const amqp = require('amqplib');
 
-function setupHandler(app) {}
+const { DB_HOST, DB_NAME, RABBIT_HOST } = process.env;
 
-function startServer() {
+function connectDB(dbHost, dbName) {
+  return mongodb.MongoClient.connect(dbHost).then(client => client.db(dbName));
+}
+
+function connectRabbit() {
+  return amqp.connect(RABBIT_HOST).then(connection => {
+    console.log('Connected to RabbitMQ');
+    return connection.createChannel();
+  });
+}
+
+function consumeViewedMessage(dbCollection, messageChannel, message) {
+  console.log('Consume message: ', message);
+  const parsedMsg = JSON.parse(message.content.toString());
+  console.log('Data in message: ', message.content.toString());
+  return dbCollection.insertOne({ videoPath: parsedMsg.videoPath }).then(() => {
+    messageChannel.ack(message);
+  });
+}
+
+function setupHandler(app, db, messageChannel) {
+  const messageCollection = db.collection('messages');
+  const videosCollection = db.collection('videosHistory');
+  app.post('/message', (req, res) => {
+    const { message } = req.body;
+    messageCollection
+      .insertOne({ message })
+      .then(() => {
+        console.log(`Save message '${message}' to history`);
+        res.json({
+          message: `[Echo] '${message}'`,
+        });
+      })
+      .catch(err => {
+        console.error(`Error for saving ${message} to db`);
+        console.error((err && err.stack) || err);
+        res.sendStatus(500);
+      });
+  });
+
+  // consume message
+  // return messageChannel.assertQueue('viewed', {}).then(() => {
+  //   console.log('assert "viewed" Queue');
+  //   return messageChannel.consume('viewed', msg =>
+  //     consumeViewedMessage(videosCollection, messageChannel, msg),
+  //   );
+  // });
+
+  return messageChannel
+    .assertQueue('viewed', 'fanout')
+    .then(() => {
+      console.log('Get a Message Exchange');
+      return messageChannel.assertQueue('', { exclusive: true });
+    })
+    .then(response => {
+      console.log('Get an anonymous queue');
+      const queueName = response.queue;
+      return messageChannel.bindQueue(queueName, 'viewed', '').then(() => {
+        console.log('Get a binding queue with name "viewed"');
+        return messageChannel.consume('viewed', msg =>
+          consumeViewedMessage(videosCollection, messageChannel, msg),
+        );
+      });
+    });
+}
+
+function startServer(db, messageChannel) {
   return new Promise(resolve => {
     const app = express();
-    setupHandler(app);
+    app.use(bodyParser.json());
+    setupHandler(app, db, messageChannel);
 
     const port = (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000;
     app.listen(port, () => {
@@ -15,8 +85,14 @@ function startServer() {
 }
 
 function main() {
-  console.log('History Service Live Re-Loading Test');
-  return startServer();
+  return connectDB(DB_HOST, DB_NAME).then(db => {
+    console.log('Success to connect with MongoDB');
+
+    return connectRabbit().then(messageChannel => {
+      console.log('Success to connect with RabbitMQ');
+      return startServer(db, messageChannel);
+    });
+  });
 }
 
 main()
--- back-end/bootstrapping-microservices/video-streaming/src/index.js
+++ back-end/bootstrapping-microservices/video-streaming/src/index.js
@@ -1,21 +1,91 @@
 const http = require('http');
 const express = require('express');
 const mongodb = require('mongodb');
+const amqp = require('amqplib');
 
-const { PORT, VIDEO_STORAGE_HOST, VIDEO_STORAGE_PORT, DB_HOST, DB_NAME } =
-  process.env;
+const {
+  PORT,
+  VIDEO_STORAGE_HOST,
+  VIDEO_STORAGE_PORT,
+  DB_HOST,
+  DB_NAME,
+  RABBIT_HOST,
+} = process.env;
 
 function connectDB() {
   return mongodb.MongoClient.connect(DB_HOST);
 }
 
-function videoStreamHandler(app, dbClient) {
+function connectRabbitWithSingleRecipient() {
+  return amqp.connect(RABBIT_HOST).then(connection => {
+    console.log('Connected to RabbitMQ');
+    return connection.createChannel();
+  });
+}
+
+function connectRabbit() {
+  return amqp.connect(RABBIT_HOST).then(connection => {
+    console.log('Connected to RabbitMQ');
+    return connection
+      .createChannel()
+      .then(messageChannel =>
+        messageChannel
+          .assertExchange('viewed', 'fanout')
+          .then(() => messageChannel),
+      );
+  });
+}
+
+function sendDirectMessage(message) {
+  const postOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const requestBody = {
+    message,
+  };
+
+  // http://{container_name} 는 토커 컴포즈의 DNS가 이름 해석
+  const request = http.request('http://history/message', postOptions, res => {
+    res.on('data', body => {
+      console.log(`BODY: ${body}`);
+    });
+    res.on('close', () => {
+      console.log('Sent a message to history microservice.');
+    });
+  });
+  request.on('error', err => {
+    console.error('Error for request');
+    console.error((err && err.stack) || err);
+  });
+
+  request.write(JSON.stringify(requestBody));
+  request.end();
+}
+
+function publishMessage(messageChannel, videoPath) {
+  console.log('Start to publish message on "viewed" queue');
+
+  const msg = { videoPath };
+  messageChannel.publish('', 'viewed', Buffer.from(JSON.stringify(msg)));
+}
+
+function videoStreamHandler(app, dbClient, messageChannel) {
   const db = dbClient.db(DB_NAME);
   const videosCollection = db.collection('videos');
 
-  app.get('/test', (req, res)=>{
-    console.log('GET /test', req);
+  app.get('/reload', (req, res) => {
+    console.log('GET /reload');
     res.send('Live Reloading Test');
+  });
+
+  app.get('/message', (req, res) => {
+    console.log('GET /message');
+    const message = req.query.msg;
+    sendDirectMessage(message ?? 'empty message');
+    res.end();
   });
 
   app.get('/video', (req, res) => {
@@ -45,6 +115,9 @@ function videoStreamHandler(app, dbClient) {
           },
         );
         req.pipe(forwardReq);
+
+        // Publish the viewed data to message queue
+        publishMessage(messageChannel, record.videoPath);
       })
       .catch(err => {
         console.error('Database query failed');
@@ -57,7 +130,14 @@ function videoStreamHandler(app, dbClient) {
 function startServer() {
   return new Promise(resolve => {
     const app = express();
-    connectDB().then(client => videoStreamHandler(app, client));
+    connectDB().then(client => {
+      console.log('Success to connect with MongoDB');
+      // return connectRabbitWithSingleRecipient().then(messageChannel => {
+      return connectRabbit().then(messageChannel => {
+        console.log('Success to connect with RabbitMQ');
+        return videoStreamHandler(app, client, messageChannel);
+      });
+    });
     app.listen(PORT, () => {
       console.log(`Streaming Service App listening on port ${PORT}`);
       resolve();
